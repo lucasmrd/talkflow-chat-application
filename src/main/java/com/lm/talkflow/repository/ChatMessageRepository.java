324f25e111a3b88ac54da1de91d78812efeccafe
Lucas Miranda <lucasvnx6@gmail.com>
2024-07-07 20:09:37 -0300

    Service e Repository de ChatMessage feitos

---
+++ src/main/java/com/lm/talkflow/repository/ChatMessageRepository.java
@@ -0,0 +1,10 @@
+package com.lm.talkflow.repository;
+
+import com.lm.talkflow.model.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findByChatId(String s);
+}
---
+++ src/main/java/com/lm/talkflow/service/ChatMessageService.java
@@ -0,0 +1,39 @@
+package com.lm.talkflow.service;
+
+import com.lm.talkflow.model.ChatMessage;
+import com.lm.talkflow.repository.ChatMessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    private final ChatMessageRepository repository;
+    private final ChatRoomService chatRoomService;
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        var chatId = chatRoomService.getChatRoomId(
+                chatMessage.getSenderId(),
+                chatMessage.getRecipientId(),
+                true
+        ).orElseThrow();
+
+        chatMessage.setChatId(chatId);
+        repository.save(chatMessage);
+
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+        var chatId = chatRoomService.getChatRoomId(
+                senderId,
+                recipientId,
+                false);
+
+        return chatId.map(repository::findByChatId).orElse(new ArrayList<>());
+    }
+}
