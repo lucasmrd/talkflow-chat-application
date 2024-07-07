ee6d7df16c4835a79a20b85d68ac17d9dfe26f27
Lucas Miranda <lucasvnx6@gmail.com>
2024-07-07 17:37:23 -0300

    primeiro commit

---
+++ src/main/java/com/lm/talkflow/TalkflowApplication.java
@@ -0,0 +1,13 @@
+package com.lm.talkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lm/talkflow/TalkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lm.talkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
