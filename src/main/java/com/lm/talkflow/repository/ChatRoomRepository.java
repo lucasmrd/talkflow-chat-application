846c96328c6b018dee511b2dbeee86c1d09c8245
Lucas Miranda <lucasvnx6@gmail.com>
2024-07-07 19:36:51 -0300

    Service e Repository de ChatRoom criado

---
+++ src/main/java/com/lm/talkflow/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package com.lm.talkflow.repository;
+
+import com.lm.talkflow.model.ChatRoom;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
+}
